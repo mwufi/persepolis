c818336e338b9790fce76af00881f51b5a30abd9
Zen T <ztang230@gmail.com>
2024-12-26 20:57:53 -0600

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HomeLink from "./components/HomeLink";
-import Link from "next/link";
-import { headers } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
